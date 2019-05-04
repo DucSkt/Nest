4b50431faba9502e66c1bdc2de59de9c4ad3a96b
Duc-Skt <ducskt11@gmail.com>
2019-05-04 17:21:19 +0700

    11

---
+++ app/src/shared/http-error.filter.ts
@@ -0,0 +1,28 @@
+import {
+    ExceptionFilter,
+    Catch,
+    ArgumentsHost,
+    Logger,
+    HttpException,
+    HttpStatus,
+  } from '@nestjs/common'; 
+  
+  @Catch()
+  export class HttpErrorFilter implements ExceptionFilter {
+    catch(exception: HttpException, host: ArgumentsHost) {
+      const ctx = host.switchToHttp();
+      const request = ctx.getRequest();
+      const response = ctx.getResponse();
+      const status = exception.getStatus();
+ 
+      const errorResponse = {
+        code: status,
+        timestamp: new Date().toLocaleDateString(),
+        path: request.url,
+        method: request.method,
+        message: exception.message.error || exception.message || null,
+      }
+
+      response.status(status).json(errorResponse);
+    }
+  }
---
+++ app/src/shared/logging.interceptor.ts
@@ -0,0 +1,22 @@
+import { Injectable, NestInterceptor, ExecutionContext, Logger, CallHandler } from '@nestjs/common'
+import { Observable } from 'rxjs'
+import { tap } from 'rxjs/operators'
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+    // log thời gian get data
+    intercept(context: ExecutionContext,  next: CallHandler) : Observable<any> {
+        const req = context.switchToHttp().getRequest();
+        const method = req.method;
+        const url = req.url;
+        const now = Date.now();
+
+
+        return next.handle().pipe(
+            tap(() => Logger.log(
+                `BBBBBBBBBBB${method} ${url} ${Date.now() - now}ms`,
+                context.getClass().name,
+            ), ),
+        );
+    }
+}
